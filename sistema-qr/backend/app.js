da98faa8fbc898aa9a4a9f3dc0752c1e5abf2580
tecnimux-dev <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-11-22 22:05:34 -0500

    Add System qrs

---
+++ sistema-qr/backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const QRCode = require('qrcode');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+
+// Cargar datos
+const elementos = require('./data.json');
+
+
+// Ruta para servir páginas de elementos
+app.get('/elemento/:id', (req, res) => {
+    const elemento = elementos.find(e => e.id === req.params.id);
+
+    if (elemento) {
+        res.send(`
+            <html>
+                <head><title>${elemento.id}</title></head>
+                <body>
+                    <h1>${elemento.id}</h1>
+                    <p>${elemento.numeroInventario}</p>
+                    <p>${elemento.marca}</p>
+                    <p>${elemento.modelo}</p>
+                    <p>${elemento.serial}</p>
+                    <p>${elemento.aula}</p>
+                </body>
+            </html>
+            `);
+    } else {
+        res.status(404).send('Elemento no encontrado');
+    }
+});
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la aplicación de QR');
+});
+
+// Inicia el servidor 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
---
+++ sistema-qr/backend/generar-qr.js
@@ -0,0 +1,20 @@
+const QRCode = require('qrcode');
+const fs = require('fs');
+const elementos = require('./data.json');
+
+const outputFolder = '../qrs';
+
+// Comprobar que la carpeta de salida exista
+if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+}
+
+elementos.forEach((elemento) => {
+    const url = `http://localhost:3000/elemento/${elemento.id}`;
+    const nombreArchivo = `${outputFolder}/qr_${elemento.id}.png`;
+
+    QRCode.toFile(nombreArchivo, url, { width: 300}, (err) => {
+        if (err) console.error(`Error generado QR para ${elemento.id}:`, err);
+        else console.log(`QR generado para ${elemento.id}: ${nombreArchivo}`);    
+    });
+ });
