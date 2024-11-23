115832a736f371a25cadac536d6078f163568d80
tecnimux-dev <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-11-22 22:31:51 -0500

    Add config System QRs

--- sistema-qr/backend/app.js
+++ sistema-qr/backend/app.js
@@ -1,14 +1,57 @@
+// const express = require('express');
+// const QRCode = require('qrcode');
+// const fs = require('fs');
+// const path = require('path');
+// const app = express();
+// const PORT = 3000;
+
+
+// // Cargar datos
+// const elementos = require('./data.json');
+
+
+// // Ruta para servir páginas de elementos
+// app.get('/elemento/:id', (req, res) => {
+//     const elemento = elementos.find(e => e.id === req.params.id);
+
+//     if (elemento) {
+//         res.send(`
+//             <html>
+//                 <head><title>${elemento.id}</title></head>
+//                 <body>
+//                     <h1>${elemento.id}</h1>
+//                     <p>${elemento.numeroInventario}</p>
+//                     <p>${elemento.marca}</p>
+//                     <p>${elemento.modelo}</p>
+//                     <p>${elemento.serial}</p>
+//                     <p>${elemento.aula}</p>
+//                 </body>
+//             </html>
+//             `);
+//     } else {
+//         res.status(404).send('Elemento no encontrado');
+//     }
+// });
+
+// // Ruta principal
+// app.get('/', (req, res) => {
+//     res.send('Bienvenido a la aplicación de QR');
+// });
+
+// // Inicia el servidor 
+// app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
+
+
 const express = require('express');
 const QRCode = require('qrcode');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
-
 
 // Cargar datos
 const elementos = require('./data.json');
-
 
 // Ruta para servir páginas de elementos
 app.get('/elemento/:id', (req, res) => {
@@ -27,7 +70,7 @@ app.get('/elemento/:id', (req, res) => {
                     <p>${elemento.aula}</p>
                 </body>
             </html>
-            `);
+        `);
     } else {
         res.status(404).send('Elemento no encontrado');
     }
@@ -38,7 +81,5 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a la aplicación de QR');
 });
 
-// Inicia el servidor 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// Exportar la app para que Vercel la use
+module.exports = app;
